8ecb2188aa562262d9e6366c2c4b61fc689e6a6f
USER <USER@USER16-PC>
2017-01-17 10:18:07 +0900

    Initial commit

---
+++ Calculator/src/Controller.java
@@ -0,0 +1,12 @@
+
+public class Controller {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculator cal = new Calculator();
+		
+		cal.add(1, 2);
+
+	}
+
+}
