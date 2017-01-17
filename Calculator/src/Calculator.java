17685374da0d91ac0f0494f1aaa679250a1f3f61
USER <USER@USER16-PC>
2017-01-17 09:45:12 +0900

    Add add funtion

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -1,9 +1,12 @@
 
 public class Calculator {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+	public int add(int num1, int num2){
+		int result=0;
+		
+		result=num1+num2;
+		
+		return result;
+		
+	}	
 }
